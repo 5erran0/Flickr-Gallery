82714cc89962dc8b3978a26d47071c3f35c7e1b5
Gerard <gserrano@gyngi.com>
2019-10-24 21:47:19 +0200

    Create Libs to centralize the libraries declaration

---
+++ buildSrc/src/main/java/com/ginzo/flickrgallery/Libs.kt
@@ -0,0 +1,34 @@
+package com.ginzo.flickrgallery
+
+object Libs {
+  object kotlin {
+    val kotlinVersion = "1.3.50"
+
+    val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion"
+
+    object gradle {
+      val core = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+      val allopen = "org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion"
+    }
+  }
+
+  val coreKtx = "androidx.core:core-ktx:1.0.2"
+
+  val constrainLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+  object androidx {
+    val appCompat = "androidx.appcompat:appcompat:1.0.2"
+  }
+
+  object google {
+    val material = "com.google.android.material:material:1.0.0"
+  }
+
+  const val junit = "junit:junit:4.12"
+}
+
+object Conf {
+  const val compileSdkVersion = 28
+  const val targetSdkVersion = 28
+  const val minSdkVersion = 21
+}
